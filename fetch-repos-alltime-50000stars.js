783796f503b39291361064fc339e03024e7d2684
Patrick Tehubijuluw <creatuluw@gmail.com>
2026-03-23 14:31:38 +0100

    Add __dirname and __filename ES module equivalents

--- fetch-repos-alltime-50000stars.js
+++ fetch-repos-alltime-50000stars.js
@@ -1,6 +1,11 @@
 import https from "https";
 import fs from "fs";
 import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Format date as YYYYMMDD
 function getDateString() {
